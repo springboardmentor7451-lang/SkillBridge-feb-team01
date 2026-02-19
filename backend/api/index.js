589e4c7af381c76722cf22e12c8ab839f8c8de48
balaji196 <chitturibalaji67@gmail.com>
2026-02-19 15:45:50 +0530

    Initial commit

---
+++ backend/api/index.js
@@ -0,0 +1,23 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import connectDB from "../config/db.js";
+import authRoutes from "../routes/authRoutes.js";
+import userRoutes from "../routes/userRoutes.js";
+
+dotenv.config();
+connectDB();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
+
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
+
+export default app;
